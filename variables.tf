fa37c8b21279bcafd1cd5638af25437688b2686a
anson inomwan <inomwanosaruese@gmail.com>
2026-07-16 18:45:46 -0400

    added new files

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Resource Group name passed into Module 1"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region passed into Module 1"
+  type        = string
+}
